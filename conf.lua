474b55d5c2f390c3431769ed971ae270974c19dc
Luis Correia <buga@loide.net>
2011-03-06 13:12:50 +0000

    Commit inicial

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	t.title = "batalha, com fisgas"
+    t.screen.width = 1024
+    t.screen.height = 768	
+end
---
+++ main.lua
@@ -0,0 +1,25 @@
+-------------------------------------------------
+-- Ultimate Sinescroller Demo		
+-- By: fleg
+-------------------------------------------------
+
+function love.load()
+--	muzac = love.audio.newSource("labyrinth.mod");
+--	love.audio.play(muzac, 0);
+end
+
+function love.update(dt)
+	
+end
+
+function love.draw()
+
+  love.graphics.setColor( 126, 126, 126, 128 )
+ 
+  love.graphics.rectangle( "fill", 0, 700, 1024, 768 )
+  love.graphics.rectangle( "fill", 500, 500, 50, 200 )
+	
+	-- love.graphics.draw(textstring, 50, i)
+end
+
+
